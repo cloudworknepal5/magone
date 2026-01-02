a4ad4a4e51e823e1a4263fd9844ad3835e098543
cloudworknepal5 <cloudworknepal5@gmail.com>
2026-01-02 15:35:04 +0545

    Update featured-script1.js

--- blogger/featured-script1.js
+++ blogger/featured-script1.js
@@ -1,37 +1,36 @@
 var numPosts = 3; 
 var snippetLength = 600; 
 
-// 1. Function to convert digits to Nepali numerals
-function toNepaliNum(num) {
-    var dict = {'0':'०','1':'१','2':'२','3':'३','4':'४','5':'५','6':'६','7':'७','8':'८','9':'९'};
-    return num.toString().replace(/[0123456789]/g, function(s) { return dict[s]; });
+// --- DATE CONVERSION UTILITIES ---
+function toNepali(n) {
+    var digits = ['०','१','२','३','४','५','६','७','८','९'];
+    return n.toString().split('').map(function(z) { return digits[z] || z; }).join('');
 }
 
-// 2. Function to format date/time into Nepali
-function getNepaliDateTime(dateString) {
-    var date = new Date(dateString);
-    if (isNaN(date.getTime())) return "";
+function getNepaliDateHtml(isoString) {
+    var d = new Date(isoString);
+    if (isNaN(d.getTime())) return "";
 
-    var days = ["आइतवार", "सोमवार", "मङ्गलवार", "बुधवार", "बिहीवार", "शुक्रवार", "शनिवार"];
-    var months = ["वैशाख", "जेठ", "असार", "साउन", "भदौ", "असोज", "कात्तिक", "मंसिर", "पुस", "माघ", "फागुन", "चैत"];
-    
-    var dayName = days[date.getDay()];
-    var dayNum = toNepaliNum(date.getDate());
-    var monthName = months[date.getMonth()];
-    var year = toNepaliNum(date.getFullYear());
-    
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    
-    // 24-hour format with leading zeros
-    var hStr = hours < 10 ? '0' + hours : hours;
-    var mStr = minutes < 10 ? '0' + minutes : minutes;
-    
-    var time = toNepaliNum(hStr) + ":" + toNepaliNum(mStr);
-    
-    return dayName + ", " + monthName + " " + dayNum + ", " + year + " | समय: " + time;
+    var days = ["आइतवार", "सोमवार", "मंगलवार", "बुधवार", "बिहीवार", "शुक्रवार", "शनिवार"];
+    var months = ["वैशाख", "जेठ", "असार", "साउन", "भदौ", "असोज", "कात्तिक", "मंसिर", "पुष", "माघ", "फागुन", "चैत"];
+
+    // Precise BS Math (Approximate)
+    var year = d.getFullYear() + 56;
+    var mon = d.getMonth() + 9;
+    var day = d.getDate() + 15;
+    if (day > 30) { day -= 30; mon++; }
+    if (mon > 12) { mon -= 12; year++; }
+
+    // 24h Time
+    var hh = d.getHours();
+    var mm = d.getMinutes();
+    var hS = hh < 10 ? '०' + toNepali(hh) : toNepali(hh);
+    var mS = mm < 10 ? '०' + toNepali(mm) : toNepali(mm);
+
+    return days[d.getDay()] + ", " + toNepali(day) + " " + months[mon-1] + " " + toNepali(year) + " | " + hS + ":" + mS;
 }
 
+// --- MAIN FEED FUNCTION ---
 function showFeatured(json) {
     var container = document.getElementById('featured-container');
     var html = '';
@@ -46,7 +45,7 @@ function showFeatured(json) {
         var entry = entries[i];
         var title = entry.title.$t;
         
-        // FAIL-PROOF LINK DETECTION
+        // --- THE LINK FIX ---
         var postUrl = "";
         for (var k = 0; k < entry.link.length; k++) {
             if (entry.link[k].rel == 'alternate') {
@@ -58,5 +57,27 @@ function showFeatured(json) {
         var authorName = entry.author[0].name.$t;
         var authorImg = entry.author[0].gd$image ? entry.author[0].gd$image.src.replace('/s113/', '/s100/') : 'https://via.placeholder.com/100';
         
-        // CONVERT DATE
-        var nepaliDateLabel = getNepaliDateTime(entry.published.$
+        // --- THE DATE FIX ---
+        var nepaliDate = getNepaliDateHtml(entry.published.$t);
+        
+        var thumb = entry.media$thumbnail ? entry.media$thumbnail.url.replace('/s72-c/', '/s1600/') : 'https://via.placeholder.com/1200x600';
+        var content = entry.summary ? entry.summary.$t : (entry.content ? entry.content.$t : "");
+        var snippet = content.replace(/<\/?[^>]+(>|$)/g, "").substring(0, snippetLength) + '...';
+
+        // --- CONSTRUCTING HTML ---
+        html += '<div class="fp-item">' +
+            '<h1 class="fp-title"><a href="' + postUrl + '">' + title + '</a></h1>' +
+            '<div class="fp-meta">' +
+                '<img class="fp-author-img" src="' + authorImg + '">' +
+                '<span><b>' + authorName + '</b></span><span>|</span><span class="nepali-date">' + nepaliDate + '</span>' +
+            '</div>' +
+            '<div class="fp-image-wrap"><a href="' + postUrl + '"><img src="' + thumb + '"></a></div>' +
+            '<div class="fp-snippet">' + snippet + '</div>' +
+            '<a href="' + postUrl + '" class="fp-readmore">थप पढ्नुहोस्</a>' +
+        '</div>';
+    }
+    container.innerHTML = html;
+}
+
+// Load Recent Posts
+document.write('<script src="/feeds/posts/default?alt=json-in-script&max-results=' + numPosts + '&callback=showFeatured"><\/script>');
